6a06737d430c5c70b02623969f1956b7f8648fe0
daniilvladimirov <dvladimirov@techlab.solutions>
2021-10-19 13:06:19 +0300

    feat(triangle): add method calculatePerimeter and validation for constructor

--- Triangle.cs
+++ Triangle.cs
@@ -7,21 +7,29 @@ namespace triangle
         private double a;
         private double b;
         private double c;
+        private double angleA;
+        private double angleB;
+        private double angleC;
 
         public Triangle()
         {
             a = 0;
             b = 0;
             c = 0;
+            angleA = 0;
+            angleB = 0;
+            angleC = 0;
         }
 
         public Triangle(double a, double b, double c)
         {
+            if (a <= 0 || b <= 0 || c <= 0) {
+                throw new Exception("a, b and c can't be zero or less than zero !"); 
+            }
             this.a = a;
             this.b = b;
             this.c = c;
         }
-
         public double getA()
         {
             return a;
@@ -34,10 +42,14 @@ namespace triangle
         {
             return c;
         }
-        public double getL(double c, double a, double b)
+        public double getL(double a, double b, double c)
         {
             double s = a + b;
             return Math.Sqrt(a * b * (s + c) * (s - c)) / s;
+        }
+        public double calculatePerimeter(double a, double b, double c)
+        {
+            return a + b + c;
         }
     }
 }
