40757f7859c394d8ca489e93c18d41c0b319a85f
daniilvladimirov <dvladimirov@techlab.solutions>
2021-10-19 12:41:40 +0300

    feat(triangle): created class triangle with methods

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace triangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri = new Triangle(1.3, 3.4, 4.0);
+            Console.WriteLine(tri.getA());
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace triangle
+{
+    public class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double getA()
+        {
+            return a;
+        }
+        public double getB()
+        {
+            return b;
+        }
+        public double getC()
+        {
+            return c;
+        }
+        public double getL(double c, double a, double b)
+        {
+            double s = a + b;
+            return Math.Sqrt(a * b * (s + c) * (s - c)) / s;
+        }
+    }
+}
