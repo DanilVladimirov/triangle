c614149b1f78d7702fc88645f504b907f150d2ca
daniilvladimirov <dvladimirov@techlab.solutions>
2021-10-25 21:50:25 +0300

    feat(triangle): caclulate area funcs

--- Triangle.cs
+++ Triangle.cs
@@ -24,32 +24,52 @@ namespace triangle
         public Triangle(double a, double b, double c)
         {
             if (a <= 0 || b <= 0 || c <= 0) {
-                throw new Exception("a, b and c can't be zero or less than zero !"); 
+                throw new Exception("a, b and c can't be zero or less than zero !");
             }
             this.a = a;
             this.b = b;
             this.c = c;
         }
+
         public double getA()
         {
             return a;
         }
+
         public double getB()
         {
             return b;
         }
+
         public double getC()
         {
             return c;
         }
-        public double getL(double a, double b, double c)
+
+        public double getL()
         {
             double s = a + b;
             return Math.Sqrt(a * b * (s + c) * (s - c)) / s;
         }
-        public double calculatePerimeter(double a, double b, double c)
+
+        public double calculatePerimeter()
         {
             return a + b + c;
+        }
+
+        public double calculateTriangleArea(double p)
+        {
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double calculateAreaWithr(double r)
+        {
+            return r * (a + b + c) / 2;
+        }
+
+        public double calculateAreaWithR(double r)
+        {
+            return (a * b * c) / 4*r;
         }
     }
 }
